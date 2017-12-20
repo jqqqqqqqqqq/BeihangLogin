2b8a72101fbfc0b79b58698fa6954ce92ef41701
rami3l <33851577+rami3l@users.noreply.github.com>
2017-12-20 23:22:20 +0800

    Add files via upload

--- buaa_traffic.py
+++ buaa_traffic.py
@@ -1,27 +1,27 @@
 """
 This is a simple script for students in BUAA to check their remaining network traffic (in bytes).
-Usage: python buaa_traffic.py [uid]
-Your uid can be found using Chrome Developer Tools, etc., when you try to log in 'gw.buaa.edu.cn.'
+Usage: python buaa_traffic.py [username]
+Your username is your ID number.
 """
 import sys
 import requests
 import json
 
-UNFORMATTED_URL = "https://gw.buaa.edu.cn:803/beihang.php?route=getPackage&uid={0}&pid=6"
-UID = sys.argv[1]
+UNFORMATTED_URL = "https://gw.buaa.edu.cn:803/beihang.php?route=getPackage&username={0}&pid=6"
+USERNAME = sys.argv[1]
 
 
-def check_by_uid(uid):
+def check_by_username(username):
     """
     uid: str
     """
-    url = UNFORMATTED_URL.format(uid)
+    url = UNFORMATTED_URL.format(username)
     res = requests.get(url)
     return res.json()
 
 
 def main():
-    json_dict = check_by_uid(UID)
+    json_dict = check_by_username(USERNAME)
     remain_bytes = json_dict["product_remain_bytes"]
     res = "{:,}".format(remain_bytes)
     print(res)
