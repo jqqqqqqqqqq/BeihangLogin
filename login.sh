b9be2b10a0f2db9258f28e7190c90c931c6d8f4e
Curtis Jiang <jqqqqqqqqqq@qq.com>
2018-06-26 11:32:13 +0800

    Feature: interface selection

--- login.sh
+++ login.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-username="你的学号"
-password="你的密码"
-useragent="Opera/9.23 (Nintendo Wii; U; ; 1038-58; Wii Internet Channel/1.0; en)"
-retry=5
-
+USERNAME="你的学号"
+PASSWORD="你的密码"
+USERAgent="Opera/9.23 (Nintendo Wii; U; ; 1038-58; Wii Internet Channel/1.0; en)"
+RETRY=5
+INTERFACE="eth0"
 #### DO NOT EDIT THIS LINE BELOW ####
 
 attempts=0
@@ -12,7 +12,7 @@ option=$1
 logout()
 {
     echo "Logging out..."
-    result=$(curl -s -k -A "$useragent" -d "action=logout&username=${username}&password=${password}&ajax=1" "https://gw.buaa.edu.cn/include/auth_action.php")
+    result=$(curl -s -k -A "$USERAGENT" --interface "$INTERFACE" -d "action=logout&username=${USERNAME}&password=${PASSWORD}&ajax=1" "https://gw.buaa.edu.cn/include/auth_action.php")
     if [[ $result =~ "logout" ]]; then
         echo "Success"
     else
@@ -28,11 +28,11 @@ urlencode()
 
 login()
 {
-    while [ $attempts -lt $retry ]
+    while [ $attempts -lt $RETRY ]
     do
 	    attempts=`expr $attempts + 1`
 	    echo "Sending login request... Attempt "$attempts
-	    result=$(curl -s -k -A "$useragent" -d "action=login&username=${username}&password={B}$(urlencode `echo -n $password|base64`)&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1" "https://gw.buaa.edu.cn:803/beihanglogin.php?ac_id=22&amp;url=https://gw.buaa.edu.cn:803/beihangview.php")
+	    result=$(curl -s -k -A "$USERAGENT" --interface "$INTERFACE"-d "action=login&username=${USERNAME}&password={B}$(urlencode `echo -n $PASSWORD|base64`)&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1" "https://gw.buaa.edu.cn:803/beihanglogin.php?ac_id=22&amp;url=https://gw.buaa.edu.cn:803/beihangview.php")
 	    if [[ $result =~ "login_ok" ]]; then
 		    echo "Login success! Your internet connection has been activated."
 #		    echo $(date)" Success" >> /tmp/login.txt
@@ -47,7 +47,7 @@ login()
 
 get_info()  # TODO
 {
-    result=$(curl -s -k -A "$useragent" -d "action=get_online_info&key=45343" "https://gw.buaa.edu.cn/include/auth_action.php")
+    result=$(curl -s -k -A "$USERAGENT" --interface "$INTERFACE"-d "action=get_online_info&key=45343" "https://gw.buaa.edu.cn/include/auth_action.php")
 }
 
 main()
