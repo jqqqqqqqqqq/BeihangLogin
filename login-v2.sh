c3788f2c000cd604a5c464ca3da1c3167c7f948b
Yunhao Tian <t123yh@outlook.com>
2021-07-06 20:50:34 +0800

    从单独的文件中读取用户名和密码

--- login-v2.sh
+++ login-v2.sh
@@ -4,8 +4,8 @@
 #####################
 # Login Information #
 #####################
-USERNAME="你的学号"
-PASSWORD="你的密码"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/account
 
 #################
 # Customization #
